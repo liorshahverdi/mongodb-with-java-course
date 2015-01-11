8416479dc0696bf982a397e8abfac414d5d5d5ab
unknown <liorshahverdi@gmail.com>
2015-01-10 21:16:11 -0500

    ok im starting to like web dev a little so what

---
+++ src/main/java/com/tengen/SparkRoutes.java
@@ -0,0 +1,16 @@
+package com.tengen;
+
+import spark.Route;
+import spark.Spark;
+/**
+ * Created by Lior on 1/10/2015.
+ */
+public class SparkRoutes {
+    public static void main(String[] args) {
+        Spark.get("/", (request, response) -> "Hello World\n");
+
+        Spark.get("/test", (request, response) -> "This is a test page\n");
+
+        Spark.get("/echo/:thing", (request, response) -> request.params(":thing"));
+    }
+}
